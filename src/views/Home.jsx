ef9240e5225e13e6924c7b29845a724e484ba4f4
iarmolla <ivan.armolla@gmail.com>
2023-07-19 16:42:16 -0300

    Update imports & components

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-import NavBar from './NavBar'
+import NavBar from '../components/NavBar'
 import actions from '../actions/characters'
 import { connect } from 'react-redux'
 import charactersSelector from '../selectors/charactersSelector'
